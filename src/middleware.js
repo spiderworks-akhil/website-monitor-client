8c5c5d739a3c48b8072efdce6ba8dcff05b95c2f
Roshan-Sw <roshan.sam@spiderworks.in>
2025-04-29 09:21:23 +0530

    middleware

--- src/middleware.js
+++ src/middleware.js
@@ -13,11 +13,16 @@ export async function middleware(request) {
   }
 
   if (!token) {
+    console.log("yes inside",token);
+    
     return NextResponse.redirect(new URL("/signin", request.url));
   }
 
   try {
     await jwtVerify(token, new TextEncoder().encode(process.env.JWT_SECRET));
+
+    console.log("yes");
+    
 
     return NextResponse.next();
   } catch (error) {
