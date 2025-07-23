b47ff3afc311e69f99fba146cebcf74625c46071
Roshan-Sw <roshan.sam@spiderworks.in>
2025-07-23 15:31:52 +0530

    updates

--- src/app/signin/page.jsx
+++ src/app/signin/page.jsx
@@ -257,7 +257,7 @@ const SignIn = () => {
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-          Sign in to your HR account
+          Sign in to Website Monitor
         </h2>
       </div>
 
